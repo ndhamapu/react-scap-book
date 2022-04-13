e0d665d7905ed7229eaaeb204da5fbc6c59e48b5
nikhil.dhamapurkar_celeritio <nikhil.dhamapurkar@celeritio.com>
2022-04-13 16:09:38 +0530

    functions how they return html

---
+++ hacker-stories/src/App.js
@@ -0,0 +1,90 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+const welcome = {
+
+  greetingMsg: 'Hello There',
+  title: 'React Sample',
+
+}
+
+const listOfAuthors = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const ListAuthors = () => (<ul>
+  {listOfAuthors.map(function (item) {
+    return <li key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+
+      </span>
+      <span>
+        {item.author}
+
+      </span>
+      <span>
+        {item.title}
+      </span>
+
+
+      <span>
+        {item.num_comments}
+
+      </span>
+    </li>;
+  })}
+
+</ul>);
+
+
+const Search = ()=> {
+  
+  const handleChange = (event) => {
+    console.log(event);
+  }
+
+    return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+);
+}
+
+
+
+function App() {
+  return (
+    <div>
+      <h1> title goes here</h1>
+      <Search />
+      <hr />
+      <ListAuthors />
+
+
+    </div>
+  );
+}
+
+function getTitle(title) {
+  return title;
+}
+
+export default App;
